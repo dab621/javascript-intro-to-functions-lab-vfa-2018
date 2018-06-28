1a9aac911dd3c752fca3810de68378e22212bf16
Danielle Bennett <dab621@stern.nyu.edu>
2018-06-28 16:56:34 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,37 +1,34 @@
-
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
+  function shout(string) {
+    return 
+  string.toUpperCase()
+}
   })
 })
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
+    function whisper (string) {
+        return
+    string.toLowerCase()
+}
   })
 })
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
+    function logShout(string){
+      console.log.restore()
+    }
   })
 })
 
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
+    function logWhisper(string){
+      console.log.restore()
+    }
   })
 })
 
